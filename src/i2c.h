bd12bca090c223c489971f43e73b14549d5c93c0
kout08 <kout08@users.noreply.github.com>
2016-02-17 20:08:14 +0900

    Add i2c functions

---
+++ src/i2c.h
@@ -0,0 +1,104 @@
+#include <unistd.h>
+
+int powInt(int, int);
+
+int i2cSetup(int devId)
+{
+	int fd;
+	char *device;
+
+	device = "/dev/i2c-1";
+
+	if((fd = open(device, O_RDWR)) < 0){
+		printf("error open device\n");
+		return -1;
+	}
+
+	if(ioctl(fd, I2C_SLAVE, devId) < 0){
+		printf("error ioCtl\n");
+		return -1;
+	}
+
+	return fd;
+}
+
+int i2cWrite(int fd, unsigned char reg, unsigned char data)
+{
+	unsigned char buf[2];
+
+	if(ioctl(fd, I2C_SLAVE, 0x40) < 0)
+		return -1;
+
+	buf[0] = reg;
+	buf[1] = data;
+
+	if((write(fd, buf, 2)) < 0)
+		return -1;
+
+	return 0;
+}
+
+/*
+	 int i2cRead(int fd, int bsize)
+	 {
+	 int ret,data,i;
+	 data = 0;
+	 unsigned char buf[bsize];
+
+	 ioctl(fd, I2C_SLAVE, 0x40);
+
+	 if((ret = read(fd, buf, (bsize*2))) != bsize*2){
+	 printf("Read only %d byte\n", ret);
+	 return -1;
+	 }
+
+	 for(i=0 ; i<(bsize*2); i++)
+	 data += buf[(bsize*2)-1-i]*powInt(16,i);
+
+	 return data;
+	 }
+ */
+
+unsigned char i2cRead(int fd, unsigned char reg)
+{
+	unsigned char buf[2];
+
+	ioctl(fd, I2C_SLAVE, 0x40);
+
+	buf[0] = reg;
+
+	if(write(fd, buf, 1) < 0)
+		return -1;
+
+	read(fd, buf, 1);
+
+	return buf[0];
+}
+
+int stdRead(int fd, int bsize)
+{
+	unsigned char buf[bsize];
+	int ret,data,i;
+	data = 0;
+
+	ioctl(fd, I2C_SLAVE, 0x40);
+
+	ret = read(fd, buf, (bsize*2));
+	if(ret != bsize*2){
+		printf("Read only %d byte\n", ret);
+		return -1;
+	}
+
+	for(i=0 ; i<(bsize*2); i++)
+		data += buf[(bsize*2)-1-i]*powInt(16,i);
+
+	return data;
+}
+
+int powInt(int x, int y)
+{
+	int i;
+	for(i=0 ; i<y ; i++)
+		x *= x;
+	return x;
+}
--- src/mruby-gpio-bbb.c
+++ src/mruby-gpio-bbb.c
@@ -1,8 +1,16 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "mruby.h"
 #include "mruby/value.h"
 #include "./set_bbbpin.h"
+//i2c header
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <linux/i2c-dev.h>
+#include "./i2c.h"
+//Use read, write
+#include <unistd.h>
 
 static mrb_value 
 f_gpio_pinmode(mrb_state *mrb, mrb_value self)
@@ -79,6 +87,46 @@ f_gpio_dread(mrb_state *mrb, mrb_value self)
 	return mrb_fixnum_value(value);
 }
 
+static mrb_value
+f_gpio_isetup(mrb_state *mrb, mrb_value self)
+{
+	mrb_int devId, ret;
+	mrb_get_args(mrb, "i", &devId);
+	ret = i2cSetup(devId);
+
+	return mrb_fixnum_value(ret);
+}
+
+static mrb_value
+f_gpio_iwrite(mrb_state *mrb, mrb_value self)
+{
+	mrb_int fd, reg, data, ret;
+	mrb_get_args(mrb, "iii", &fd, &reg, &data);
+	ret = i2cWrite(fd, reg, data);
+
+	return mrb_fixnum_value(ret);
+}
+
+static mrb_value
+f_gpio_iread(mrb_state *mrb, mrb_value self)
+{
+	mrb_int fd, reg, ret;
+	mrb_get_args(mrb, "ii", &fd, &reg);
+	ret = i2cRead(fd, reg);
+
+	return mrb_fixnum_value(ret);
+}
+
+static mrb_value
+f_gpio_stdread(mrb_state *mrb, mrb_value self)
+{
+	mrb_int fd, bsize, ret;
+	mrb_get_args(mrb, "ii", &fd, &bsize);
+	ret = stdRead(fd, bsize);
+
+	return mrb_fixnum_value(ret);
+}
+
 void
 mrb_mruby_gpio_bbb_gem_init(mrb_state* mrb)
 {
@@ -93,9 +141,15 @@ mrb_mruby_gpio_bbb_gem_init(mrb_state* mrb)
 	mrb_define_module_function(mrb, c, "pinMode", f_gpio_pinmode, MRB_ARGS_REQ(2));
 	mrb_define_module_function(mrb, c, "digitalWrite", f_gpio_dwrite, MRB_ARGS_REQ(2));
 	mrb_define_module_function(mrb, c, "digitalRead", f_gpio_dread, MRB_ARGS_REQ(1));
+	
+	//i2c
+	mrb_define_module_function(mrb, c, "i2cSetup", f_gpio_isetup, MRB_ARGS_REQ(1));
+	mrb_define_module_function(mrb, c, "i2cWrite", f_gpio_iwrite, MRB_ARGS_REQ(3));
+	mrb_define_module_function(mrb, c, "i2cRead", f_gpio_iread, MRB_ARGS_REQ(2));
+	mrb_define_module_function(mrb, c, "stdRead", f_gpio_stdread, MRB_ARGS_REQ(2));
 }
 
-void
+	void
 mrb_mruby_gpio_bbb_gem_final(mrb_state* mrb)
 {
 }
